b20e737b3ca3904779de2c6f3f54ef68a3880bee
solmin <solminkim03@naver.com>
2021-07-19 10:24:13 +0900

    first commit

---
+++ Program/src/work/practice/consoleChat.java
@@ -0,0 +1,15 @@
+package work.practice;
+
+public class consoleChat {
+	public static void main(String[] args) {
+		final int SERVER_PORT = 5000;
+		
+	}
+}
+
+/* serversocket : 서버 역할을 하는 소켓 객체
+	클라이언트의 연결 요청을 기다리면서 연결 요청에 대한 수락을 담당
+	socket : 클라이언와 서버 간의 통신을 직접 담당
+	
+	InetAddress : ip 주소를 표현할 때 사용하는 클래스 
+*/
